fd21cd5b305beee4e29155d6fe415d28eaa5977b
Om Shukla <omshukla2403@gmail.com>
2025-05-19 22:25:20 +0530

    Initial commit

---
+++ client/src/components/LatestCollections.jsx
@@ -0,0 +1,34 @@
+import React, { useContext, useState, useEffect } from 'react'
+import { ShopContext } from '../context/ShopContext'
+import Title from './Title';
+
+const LatestCollections = () => {
+
+    const { products } = useContext(ShopContext);
+    const [lastestProducts, setLatestProducts] = useState([]);
+
+    useEffect(()=>{
+        setLatestProducts(products.slice(0, 10));
+    }, [])
+
+  return (
+    <div className='my-10'>
+        <div className='text-center py-8 text-3xl'>
+            <Title text1={'LATEST'} text2={'COLLECTIONS'}/>
+            <p className='w-3/4 m-auto text-xs sm:text-sm md:text-base text-gray-600'>
+                Here are some of our new best collections that we have added after huge demands
+            </p>
+        </div>
+
+        {/* Rendering Products */}
+        <div className='grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4 gap-y-6'>
+            {
+
+            }
+        </div>
+        
+    </div>
+  )
+}
+
+export default LatestCollections
