0f7b337c998a180e004f30bc418d53aa5269da54
Om Shukla <omshukla2403@gmail.com>
2025-05-26 11:20:41 +0530

    Completed Frontend

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -1,8 +1,46 @@
 import React from 'react'
+import Title from '../components/Title'
+import NewsLetterBox from '../components/NewsLetterBox'
+import { assets } from '../assets/frontend_assets/assets'
 
 const About = () => {
   return (
-    <div>About</div>
+    <div>
+      <div className='text-2xl text-center pt-8 border-t'>
+        <Title text1={"ABOUT"} text2={"US"}/>
+      </div>
+
+      <div className='my-10 flex flex-col md:flex-row gap-16'>
+        <img src={assets.about_img} className='w-full max-w-[450px]' />
+        <div className='flex flex-col justify-center gap-6 md:w-2/4 text-gray-600'>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium, veniam hic ullam quidem labore fuga. Quia praesentium enim dignissimos odit.</p>
+          <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Repellendus, perferendis.</p>
+          <b className='text-gray-800'>Our Mission</b>
+          <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sint porro reiciendis perferendis soluta qui consequuntur.</p>
+        </div>
+      </div>
+
+      <div className='text-xl py-4'>
+        <Title text1={'WHY'} text2={"CHOOSE US"} />
+      </div>
+
+      <div className='flex flex-col md:flex-row text-sm mb-20'>
+          <div className='border px-10 md:px-16 py-8 sm:py-20 flex flex-col gap-5'>
+            <b>Quality Assurance:</b>
+            <p className='text-gray-600'>We meticulously select and vet each product to ensure it meets our stringent quality standards</p>
+          </div>
+          <div className='border px-10 md:px-16 py-8 sm:py-20 flex flex-col gap-5'>
+            <b>Convienience:</b>
+            <p className='text-gray-600'>With our user friendly interface and hassle-free ordering process, shipping has been made easy</p>
+          </div>
+          <div className='border px-10 md:px-16 py-8 sm:py-20 flex flex-col gap-5'>
+            <b>Expectional Customer Service:</b>
+            <p className='text-gray-600'>Our Professionals are here to assist you the way, ensuring your satisfaction is out top priority.</p>
+          </div>
+        </div>
+
+        <NewsLetterBox />
+    </div>
   )
 }
 
--- client/src/pages/Contact.jsx
+++ client/src/pages/Contact.jsx
@@ -1,8 +1,29 @@
 import React from 'react'
+import Title from '../components/Title'
+import NewsLetterBox from '../components/NewsLetterBox'
+import { assets } from '../assets/frontend_assets/assets'
 
 const Contact = () => {
   return (
-    <div>Contact</div>
+    <div>
+      <div className='text-center text-2xl border-t pt-10'>
+        <Title text1={'CONTACT'} text2={'US'}/>
+      </div>
+
+      <div className='my-10 flex flex-col justify-center md:flex-row gap-10 mb-28'>
+        <img src={assets.contact_img} className='w-full md:max-w-[480px]' alt="" />
+        <div className='flex flex-col justify-center items-start gap-6'>
+          <p className='font-semibold text-xl text-gray-600'>Our Store</p>
+          <p className='text-gray-500'>Random gali 420 <br /> Hardoi, UP, India</p>
+          <p className='text-gray-500'>Tel: 123456789 <br /> Email: pagalkutta@random.com</p>
+          <p className='font-semibold text-xl text-gray-600'>Careers ar Forever</p>
+          <p className='text-gray-500'>Learn more about our teams and job openings.</p>
+          <button className='border border-black px-8 py-4 text-sm hover:bg-black hover:text-white transition-all duration-500'>Explore Jobs </button>
+        </div>
+      </div>
+
+      <NewsLetterBox />
+    </div>
   )
 }
 
